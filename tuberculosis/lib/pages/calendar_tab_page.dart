21f8b886ba3670948a09bc09c41c505d26e6376f
Julius van Dijk <info@magnifisite.nl>
2018-03-31 16:42:15 +0200

    Add month calendar and add state to main app

--- tuberculosis/lib/pages/calendar_tab_page.dart
+++ tuberculosis/lib/pages/calendar_tab_page.dart
@@ -1,3 +1,4 @@
+import 'package:Tubuddy/calendar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:Tubuddy/pages/tab_page.dart';
@@ -7,9 +8,14 @@ class CalendarTabPage extends StatelessWidget implements TabPage {
   static final title = const Text("Calendar");
   static final Icon icon = const Icon(CupertinoIcons.home);
 
+  final DateTime today;
+  final ValueChanged<DateTime> onDateSelected;
+
+  CalendarTabPage(this.today, [this.onDateSelected]);
+
   @override
   Widget build(BuildContext context) {
-    return const Icon(Icons.do_not_disturb_alt);
+    return new Calendar(isExpandable: true, onDateSelected: onDateSelected);
   }
 
   @override
