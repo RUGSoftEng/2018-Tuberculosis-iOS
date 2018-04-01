319592068effb1fcacb7c555118638fa628d2966
Noam Drong <noam@drong.net>
2018-04-01 21:01:54 +0200

    Refactor login page

--- tuberculosis/lib/pages/login_page.dart
+++ tuberculosis/lib/pages/login_page.dart
@@ -39,62 +39,78 @@ class LoginPageState extends State<LoginPage> {
                             child: new Center(child: _loginWidget())))))));
   }
 
+  void _showInSnackbar(String val) {
+    _scaffoldKey.currentState
+        .showSnackBar(new SnackBar(content: new Text(val)));
+  }
+
   Widget _loginWidget() {
+    final logo = new Image.asset('graphics/logo.png', height: 200.0);
+
+    final usernameField = new TextFormField(
+      decoration: new InputDecoration(
+          hintText: "Username",
+          contentPadding: const EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+          border: new OutlineInputBorder(
+              borderRadius: new BorderRadius.circular(32.0))),
+      validator: _validateUsername,
+      onSaved: (val) => _username = val,
+      keyboardType: TextInputType.emailAddress,
+      autocorrect: false,
+    );
+
+    final passwordField = new TextFormField(
+      decoration: new InputDecoration(
+          hintText: "Password",
+          contentPadding: const EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+          border: new OutlineInputBorder(
+              borderRadius: new BorderRadius.circular(32.0))),
+      validator: _validatePassword,
+      onSaved: (val) => _password = val,
+      obscureText: true,
+    );
+
+    final loginButton = new Padding(
+        padding: new EdgeInsets.only(top: 16.0),
+        child: new Material(
+            borderRadius: new BorderRadius.circular(30.0),
+            shadowColor: Colors.lightBlueAccent.shade100,
+            elevation: 5.0,
+            child: new MaterialButton(
+              minWidth: 200.0,
+              height: 42.0,
+              onPressed: _processForm,
+              child: new Text(
+                "Log In",
+                style: const TextStyle(color: Colors.white),
+              ),
+              color: Colors.lightBlueAccent,
+            )));
+
+    final forgottenPasswordButton = new FlatButton(
+        child: const Text(
+          "Forgotten Password",
+          style: const TextStyle(color: Colors.black54),
+        ),
+        onPressed: () => _showInSnackbar(
+            "Implementation needed.") // TODO: implement forgotten password function.
+        );
+
     final loginForm = new Form(
         key: _formKey,
         child: new Column(
           mainAxisAlignment: MainAxisAlignment.center,
           mainAxisSize: MainAxisSize.min,
           children: <Widget>[
-            new Image.asset('graphics/logo.png', scale: 0.5),
-            new TextFormField(
-              decoration: new InputDecoration(
-                  hintText: "Username",
-                  contentPadding:
-                      const EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
-                  border: new OutlineInputBorder(
-                      borderRadius: new BorderRadius.circular(32.0))),
-              validator: _validateUsername,
-              onSaved: (val) => _username = val,
-              keyboardType: TextInputType.emailAddress,
-              autocorrect: false,
-            ),
+            logo,
+            usernameField,
             const SizedBox(height: 12.0),
-            new TextFormField(
-              decoration: new InputDecoration(
-                  hintText: "Password",
-                  contentPadding:
-                      const EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
-                  border: new OutlineInputBorder(
-                      borderRadius: new BorderRadius.circular(32.0))),
-              validator: _validatePassword,
-              onSaved: (val) => _password = val,
-              obscureText: true,
-            ),
+            passwordField,
             const SizedBox(height: 6.0),
-            new Padding(
-                padding: new EdgeInsets.only(top: 16.0),
-                child: new Material(
-                    borderRadius: new BorderRadius.circular(30.0),
-                    shadowColor: Colors.lightBlueAccent.shade100,
-                    elevation: 5.0,
-                    child: new MaterialButton(
-                      onPressed: _processForm,
-                      child: new Text(
-                        "Log In",
-                        style: const TextStyle(color: Colors.white),
-                      ),
-                      color: Colors.lightBlueAccent,
-                    ))),
+            loginButton,
             const SizedBox(height: 12.0),
-            new FlatButton(
-              child: const Text(
-                'Forgot password?',
-                style: const TextStyle(color: Colors.black54),
-              ),
-              onPressed: () =>
-                  null, // TODO: implement forgotten password function.
-            )
+            forgottenPasswordButton,
+            const SizedBox(height: 50.0)
           ],
         ));
     return loginForm;
@@ -108,11 +124,6 @@ class LoginPageState extends State<LoginPage> {
   String _validateUsername(String val) {
     if (val.isEmpty) return "Please enter a username.";
     return null;
-  }
-
-  void _showInSnackbar(String val) {
-    _scaffoldKey.currentState
-        .showSnackBar(new SnackBar(content: new Text(val)));
   }
 
   void _processForm() {
