e74317dcf3941272ec3926c1a5b14956e19b171a
Julius van Dijk <info@magnifisite.nl>
2018-04-02 16:25:17 +0200

    Auto hide keyboard and change column to ListView

--- tuberculosis/lib/pages/login_page.dart
+++ tuberculosis/lib/pages/login_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
@@ -99,9 +100,9 @@ class LoginPageState extends State<LoginPage> {
 
     final loginForm = new Form(
         key: _formKey,
-        child: new Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          mainAxisSize: MainAxisSize.min,
+        child: new ListView(
+//          mainAxisAlignment: MainAxisAlignment.center,
+//          mainAxisSize: MainAxisSize.min,
           children: <Widget>[
             logo,
             usernameField,
@@ -131,6 +132,7 @@ class LoginPageState extends State<LoginPage> {
     final form = _formKey.currentState;
     if (form.validate()) {
       form.save();
+      SystemChannels.textInput.invokeMethod('TextInput.hide');
       _handleLogin();
     }
   }
