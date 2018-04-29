9c7dcd519f7b91577b06a3f543ff65564550774b
Julius van Dijk <info@magnifisite.nl>
2018-04-29 17:56:54 +0200

    Implement basic quiz functionality

--- tuberculosis/lib/pages/information_tab_page.dart
+++ tuberculosis/lib/pages/information_tab_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter/widgets.dart';
 import 'package:Tubuddy/pages/tab_page.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'package:Tubuddy/quiz/quiz.dart';
 
 class InformationTabPage extends StatelessWidget implements TabPage {
   static final Text title = const Text("Information");
@@ -67,27 +68,60 @@ class VideoScreen extends StatelessWidget {
       "https://www.youtube.com/watch?v=yR51KVF4OX0",
       "https://www.youtube.com/watch?v=yR51KVF4OX0"
     ];
+    List<Question> questions = <Question>[
+      const Question("Hoi"),
+      const Question("Vraag 2"),
+      new Question("ASdadasda", answers: ["asdsadsadasadsasaasdas", "bgbgbgbgbgbg", "cdiasdihsjsdjfhskjfhskdfhkshjkjkhksfdhjkh"], correctAnswer: 2)
+    ];
     return new CupertinoPageScaffold(
         navigationBar: new CupertinoNavigationBar(
           middle: new Text(_topic),
         ),
         child: new Material(
             child: new SafeArea(
-                child: new GridView.count(
-                    primary: false,
-                    padding: const EdgeInsets.all(20.0),
-                    crossAxisSpacing: 10.0,
-                    crossAxisCount: 2,
-                    children: sampleData.map((String url) {
-                      return new GridTile(
-                          child: new GestureDetector(
-                              onTap: () => _openVideo(url),
-                              child: new Image.network(
-                                  "http://img.youtube.com/vi/" +
-                                      getIdFromUrl(url) +
-                                      "/hqdefault.jpg",
-                                  fit: BoxFit.cover)));
-                    }).toList()))));
+                child: new Column(
+                  children: [
+                    new GestureDetector(
+                      onTap: () => _openVideo(sampleData[0]),
+                      child: new Image.network(
+                          "http://img.youtube.com/vi/" +
+                              getIdFromUrl(sampleData[0]) +
+                              "/hqdefault.jpg",
+                          fit: BoxFit.cover),
+                    ),
+//                    new Expanded(child: new GridView.count(
+//                      // Create a grid with 2 columns. If you change the scrollDirection to
+//                      // horizontal, this would produce 2 rows.
+//                      crossAxisCount: 2,
+//                      childAspectRatio: 4.0,
+//                      // Generate 100 Widgets that display their index in the List
+//                      children: new List.generate(100, (index) {
+//                        return new Center(
+//                          child: new Text(
+//                            'Item $index',
+//                            style: Theme.of(context).textTheme.headline,
+//                          ),
+//                        );
+//                      }),
+//                    )),
+                    new QuizWidget(questions)
+                  ],
+                ))));
+//                child: new GridView.count(
+//                    primary: false,
+//                    padding: const EdgeInsets.all(20.0),
+//                    crossAxisSpacing: 10.0,
+//                    crossAxisCount: 2,
+//                    children: sampleData.map((String url) {
+//                      return new GridTile(
+//                          child: new GestureDetector(
+//                              onTap: () => _openVideo(url),
+//                              child: new Image.network(
+//                                  "http://img.youtube.com/vi/" +
+//                                      getIdFromUrl(url) +
+//                                      "/hqdefault.jpg",
+//                                  fit: BoxFit.cover)));
+//                    }).toList()))));
   }
 
   String getIdFromUrl(String url) {
---
+++ tuberculosis/lib/quiz/quiz_widget.dart
@@ -0,0 +1,116 @@
+import 'package:Tubuddy/quiz/question.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+typedef void CheckAnswerCallback(int check);
+class _QuestionWidget extends StatelessWidget {
+  final Question _question;
+  final CheckAnswerCallback onPressed;
+
+  _QuestionWidget(this._question, this.onPressed);
+
+  @override
+  Widget build(BuildContext context) {
+    // Create basic elements (title + question)
+    List<Widget> c = [
+      new Center(child: new Text("Quiz", style: Theme.of(context).textTheme.subhead)),
+      new Center(child: new Text(_question.question, style: Theme.of(context).textTheme.headline)),
+    ];
+    // Add answer input elements
+    List<Widget> answerWidgets = [];
+    for (var i = 0; i < _question.answers.length; i++) {
+      answerWidgets.add(new CupertinoButton(child: new Text(_question.answers[i]), onPressed: () {
+        onPressed(i);
+      }));
+    }
+    c.add(new Expanded(child: new ListView(children: answerWidgets,)));
+
+    return new Expanded(child: new Column(children: c,));
+  }
+}
+
+class _QuestionCheckPage extends StatelessWidget {
+  final String message;
+  final bool isCorrect;
+  final VoidCallback onNextClicked;
+
+  _QuestionCheckPage(this.message, this.isCorrect, this.onNextClicked);
+
+  @override
+  Widget build(BuildContext context) {
+    return new Expanded(child: new Column(children: [
+      new Text(message, style: Theme.of(context).textTheme.headline.copyWith(color: isCorrect ? Colors.green : Colors.red)),
+      new CupertinoButton(child: new Text("OK"), onPressed: onNextClicked)
+    ], crossAxisAlignment: CrossAxisAlignment.center));
+  }
+}
+
+class _QuizFinished extends StatelessWidget {
+  final int correct;
+  final int total;
+
+  _QuizFinished(this.correct, this.total);
+
+  @override
+  Widget build(BuildContext context) {
+    return new Center(child: new Text("Je had $correct vragen van de $total goed.", style: Theme.of(context).textTheme.headline));
+  }
+}
+
+class QuizWidget extends StatefulWidget {
+  final List<Question> _questions;
+
+  const QuizWidget(this._questions);
+
+  @override
+  State<StatefulWidget> createState() => new _QuizState();
+}
+
+class _QuizState extends State<QuizWidget> {
+
+  int currentQuestion = 0;
+  int correct = 0;
+  bool showCheckPage = false;
+  String checkMessage = "";
+  bool lastQuestionCorrect = false;
+  bool showFinishPage = false;
+
+  void _checkAnswer(int idx) {
+    setState(() {
+      if (widget._questions[currentQuestion].correctAnswer == idx) {
+        correct++;
+        lastQuestionCorrect = true;
+        checkMessage = "Goed!";
+      } else {
+        lastQuestionCorrect = false;
+        checkMessage = "Fout. Het goede antwoord is ${widget._questions[currentQuestion].answers[widget._questions[currentQuestion].correctAnswer]}";
+      }
+      showCheckPage = true;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (showFinishPage) {
+      return new _QuizFinished(correct, widget._questions.length);
+    }
+
+    if (showCheckPage) {
+      return new _QuestionCheckPage(checkMessage, lastQuestionCorrect, () {
+        setState(() {
+          showCheckPage = false;
+          currentQuestion = currentQuestion + 1;
+          if (currentQuestion == widget._questions.length) {
+            showFinishPage = true;
+          }
+        });
+      });
+    } else {
+      return new Expanded(child: new Column(children: [
+        new _QuestionWidget(widget._questions[currentQuestion], (i) => _checkAnswer(i)),
+        new Align(alignment: Alignment.bottomCenter, child: new Text("Vraag ${currentQuestion+1}/${widget._questions.length}"))
+      ]));
+    }
+  }
+
+}
