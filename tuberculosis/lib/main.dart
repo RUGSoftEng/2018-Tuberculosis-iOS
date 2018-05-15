9b655b68108cbb250d3c3d61f011989dab9f56f3
Noam Drong <noam@drong.net>
2018-05-15 21:16:07 +0200

    Implement medication list on calendar page

--- tuberculosis/lib/main.dart
+++ tuberculosis/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:Tubuddy/pages/medication_tab_page.dart';
 import 'package:Tubuddy/pages/pages.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
@@ -55,8 +56,12 @@ class _MyAppState extends State<MyApp> {
               TabPage pageContent;
               switch (index) {
                 case 0:
+                  List<MedicationItem> pills = dummyMedicationData;
+                  if (selectedDate.day != (new DateTime.now()).day) {
+                    pills = [new MedicationItem("Fissa", "Any Time", 1)];
+                  }
                   pageContent = new CalendarTabPage(
-                      selectedDate,
+                      selectedDate, pills,
                           (DateTime date) => setState(() {
                         selectedDate = date;
                       }));
